aa3a22abd10b2b95a9b84d6986b10cf47863fb80
MissH0pe <87160591+MissH0pe@users.noreply.github.com>
2021-07-29 21:33:19 -0600

    custom display size setup

--- StandaloneTools/Building_Generator/CofDBuildingGenerator.py
+++ StandaloneTools/Building_Generator/CofDBuildingGenerator.py
@@ -39,36 +39,75 @@ class MyWidget(QtWidgets.QWidget):
         self.setWindowTitle('Chronicles of Darkness Building Generator')
         self.layout = QtWidgets.QGridLayout(self)
 
-        self.display = []
+        # newAction = QtGui.QAction('&New', self)
+        # # newAction.setShortcut('Ctrl+N')
+        # newAction.setStatusTip('New Building')
+        # newAction.triggered.connect(self.new)
+        #
+        # QtWidgets.QStatusBar()
+        #
+        # self.menubar = QtWidgets.QMenuBar()
+        # self.fileMenu = self.menubar.addMenu('&File')
+        # self.fileMenu.addAction(newAction)
+        # self.layout.addWidget(self.menubar, 0, 0, 0, 20)
 
-        self.display.append([])
+        self.gendisplaybutton = QtWidgets.QPushButton()
+        self.gendisplaybutton.setText('Regenerate Display Size')
+        self.gendisplaysizextext = QtWidgets.QLabel()
+        self.gendisplaysizextext.setText('X Dimension: ')
+        self.gendisplaysizexbox = QtWidgets.QLineEdit()
+        self.gendisplaysizeytext = QtWidgets.QLabel()
+        self.gendisplaysizeytext.setText('Y Dimension: ')
+        self.gendisplaysizeybox = QtWidgets.QLineEdit()
 
-        pixMap = QtGui.QPixmap.fromImage('resources/blank.png')
+        self.gendisplaybutton.clicked.connect(self.gendisplay)
 
-        for xit in range(xrooms):
-            self.display[0].append([])
-            for yit in range(yrooms):
-                self.display[0][xit].append(QtWidgets.QLabel())
-                self.display[0][xit][yit].setPixmap( pixMap )
-                self.layout.addWidget(self.display[0][xit][yit], yit, xit)
+        self.layout.addWidget(self.gendisplaybutton, 0, 0)
+        self.layout.addWidget(self.gendisplaysizextext, 0, 1)
+        self.layout.addWidget(self.gendisplaysizexbox, 0, 2)
+        self.layout.addWidget(self.gendisplaysizeytext, 0, 3)
+        self.layout.addWidget(self.gendisplaysizeybox, 0, 4)
 
-        twallt = QtGui.QPixmap.fromImage('resources/twallt.png')
+    def gendisplay(self):
+        # print('test')
+        if self.gendisplaysizexbox.text() != '' and self.gendisplaysizeybox.text() != '':
+            self.display = []
 
-        self.display.append([])
+            self.display.append([])
 
-        for it in range(xrooms):
-            self.display[1].append(QtWidgets.QLabel())
-            self.display[1][it].setPixmap(twallt)
-            self.layout.addWidget(self.display[1][it], 0, it)
+            pixMap = QtGui.QPixmap.fromImage('resources/blank.png')
 
-        lwallt = QtGui.QPixmap.fromImage('resources/lwallt.png')
+            for xit in range(int(float(self.gendisplaysizexbox.text()))):
+                self.display[0].append([])
+                for yit in range(int(float(self.gendisplaysizeybox.text()))):
+                    self.display[0][xit].append(QtWidgets.QLabel())
+                    self.display[0][xit][yit].setPixmap( pixMap )
+                    self.layout.addWidget(self.display[0][xit][yit], yit + 2, xit + 5)
 
-        self.display.append([])
+            twallt = QtGui.QPixmap.fromImage('resources/twallt.png')
 
-        for it in range(xrooms):
-            self.display[2].append(QtWidgets.QLabel())
-            self.display[2][it].setPixmap(lwallt)
-            self.layout.addWidget(self.display[2][it], it, 0)
+            self.display.append([])
+
+            for it in range(xrooms):
+                self.display[1].append(QtWidgets.QLabel())
+                self.display[1][it].setPixmap(twallt)
+                self.layout.addWidget(self.display[1][it], 2, it + 5)
+
+            lwallt = QtGui.QPixmap.fromImage('resources/lwallt.png')
+
+            self.display.append([])
+
+            for it in range(xrooms):
+                self.display[2].append(QtWidgets.QLabel())
+                self.display[2][it].setPixmap(lwallt)
+                self.layout.addWidget(self.display[2][it], it + 2, 5)
+
+    # def new(self):
+    #     w = QtWidgets.QWidget()
+    #
+    #     w.setWindowTitle('Chronicles of Darkness Building Generator')
+    #
+    #     layout = QtWidgets.QGridLayout(w)
 
 xdim = int(sys.argv[1])
 ydim = int(sys.argv[2])
